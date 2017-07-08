e73da4f15edf96963175640c225286723a6a50ae
Marijo Alega <mee.hyun344@gmail.com>
2017-07-08 13:15:02 +0800

    Alega

---
+++ Alega/functions-arguments.js
@@ -0,0 +1,4 @@
+function math (una, pangalawa, third) {
+    return ((pangalawa * third + una))
+}
+console.log(math(53 ,61, 67));
---
+++ Alega/rounding-numbers.js
@@ -0,0 +1,3 @@
+var roundUp = 1.5;
+var roundUp = Math.round(roundUp);
+console.log(roundUp);
