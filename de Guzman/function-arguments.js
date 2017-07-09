5988db7fe64b881a97dd228b15ebbc85342a41c2
Rez de Guzman <johnrezdeguzman9123@gmail.com>
2017-07-10 00:00:12 +0800

    JavaScripting HW

---
+++ de Guzman/function-arguments.js
@@ -0,0 +1,6 @@
+function math(Bruce, Wayne, Batman) {
+    return (Wayne * Batman) + Bruce;
+
+}
+
+console.log(math(53, 61, 67));
---
+++ de Guzman/variable.js
@@ -0,0 +1,5 @@
+var example;
+
+example = 'some string';
+
+console.log(example);
