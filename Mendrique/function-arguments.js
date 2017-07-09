9d00d66f56d240447d0f733cedd0322269962f47
ShainaBlaze <shianersie.flamel@yahoo.com>
2017-07-09 16:53:39 +0800

    Javascript
    Javascripting

---
+++ Mendrique/function-arguments.js
@@ -0,0 +1,9 @@
+var numberOne;
+var numberTwo;
+var numberThree;
+
+function math(numberOne, numberTwo, numberThree) {
+	return (numberTwo * numberThree) + numberOne;
+}
+
+console.log(math(53, 61, 67));
