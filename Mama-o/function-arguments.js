48ad820feec774ac5335fb1e5ba50f26ceebacb8
Princess Steevelyn <princesssteevelyn@gmail.com>
2017-07-10 20:06:59 +0800

    Mama-o

---
+++ Mama-o/function-arguments.js
@@ -0,0 +1,7 @@
+function math  (math,iss,sick){
+	return iss*sick+math;
+}
+var hi = 53;
+var hello = 61;
+var bye= 67;
+console.log(math(hi,hello,bye));
---
+++ Mama-o/functions.js
@@ -0,0 +1,5 @@
+var foods = 'bananas';
+function eat(food){
+	return food + ' tasted really good.';
+}
+console.log(eat(foods));
