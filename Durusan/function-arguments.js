125eab725eeeb7f4127cba152c6048e569f5ab5f
Michelle Ellaine Durusan <ms.durusan0619@gmail.com>
2017-07-10 10:10:21 +0800

    Durusan

---
+++ Durusan/function-arguments.js
@@ -0,0 +1,4 @@
+function math (icebear, grizzly, panda){
+	return (grizzly * panda) + icebear;
+}
+console.log(math(53, 61, 67));
---
+++ Durusan/rounding-numbers.js
@@ -0,0 +1,5 @@
+var roundUp;
+
+var roundUp = 1.5;
+rounded = Math.round(roundUp);
+console.log(rounded);
