77f7c15e4e14f828e99629642ff7b921e9f39ce0
matthewnanoz <johnmatthewnanoz@gmail.com>
2017-07-09 17:48:27 -0700

    javascripting

---
+++ Nañoz/function-arguments.js
@@ -0,0 +1,5 @@
+function math(firstArg,secondArg,thirdArg){
+var product = secondArg * thirdArg;
+return product + firstArg;
+}
+console.log(math(53,61,67));
---
+++ Nañoz/if-statement.js
@@ -0,0 +1,6 @@
+var fruit = 'orange';
+n = fruit.length;
+if (n > 5) {
+console.log('The fruit name has more than five characters.');
+}
+else{console.log('The fruit name has five characters or less.');}
