0385f88e7d4fb365ea156a9c5d14cbde9b7ffca5
Shann Angeli <shannangeli.festin@gmail.com>
2017-07-11 16:21:09 +0800

    Festin

---
+++ Festin/function-arguments.js
@@ -0,0 +1,8 @@
+var firstnum = 53;
+var secondnum = 61;
+var thirdnum = 67;
+
+function math (firstArg, secondArg, thirdArg) {
+	return secondArg * thirdArg + firstArg;
+}
+	console.log(math(firstnum, secondnum, thirdnum));
---
+++ Festin/functions.js
@@ -0,0 +1,6 @@
+var fruit = 'bananas';
+function eat (food) {
+	return food + ' tasted really good.';
+}
+
+console.log(eat(fruit));
